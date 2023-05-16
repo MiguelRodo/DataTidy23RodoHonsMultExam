b5fa31a7323745df9b2fb5285d6a6f101f046295
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-05-16 04:50:54 +0000

    State before patch bump to 1.0.1: Attempt to provision data correctly

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Luminx response data
+#'
+#' Data containing responses to Mtb antigens from the Luminex assay.
+#'
+#' @format ## `who`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+"lum_tbl"
